3b07398068d1895f389cd0aa8f06e188a0183541
YeungKC <I@YeungKC.com>
2022-07-20 21:57:21 +0800

    use ios date format

--- src/lib/helper/readableDate.ts
+++ src/lib/helper/readableDate.ts
@@ -1,11 +1,9 @@
 import { TIME_ZONE } from '../constants';
 
 export const readableDate = (dateText: string) =>
-	new Date(dateText)
-		.toLocaleDateString('iso', {
-			timeZone: TIME_ZONE,
-			month: '2-digit',
-			day: '2-digit',
-			year: 'numeric'
-		})
-		.replace(/\//g, '-');
+	new Date(dateText).toLocaleDateString('sv', {
+		timeZone: TIME_ZONE,
+		month: '2-digit',
+		day: '2-digit',
+		year: 'numeric'
+	});
