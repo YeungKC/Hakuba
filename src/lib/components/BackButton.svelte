027bac91bb33a61c25882ea0eaa672443edf44b6
YeungKC <I@YeungKC.com>
2022-07-20 23:56:23 +0800

    improve back button

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,9 +1,5 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-</script>
-
-<button
-	on:click={() => goto('/')}
+<a
+	sveltekit:prefetch
 	class="absolute left-0 top-0 -ml-4 -translate-y-3/4 self-start px-4 py-3 text-xs font-semibold opacity-0 transition group-hover:opacity-100 [@media(any-hover:none)]:opacity-100"
-	>POSTS /</button
+	href="/">POSTS /</a
 >
