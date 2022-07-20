62e4d93ac7f4cae74fed830ce006166418e02de3
YeungKC <I@YeungKC.com>
2022-07-21 00:02:40 +0800

    fix back button style

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,5 +1,5 @@
 <a
 	sveltekit:prefetch
-	class="absolute left-0 top-0 -ml-4 -translate-y-3/4 self-start px-4 py-3 text-xs font-semibold opacity-0 transition group-hover:opacity-100 [@media(any-hover:none)]:opacity-100"
+	class="absolute left-0 top-0 -ml-4 -translate-y-3/4 self-start px-4 py-3 text-xs font-semibold tracking-normal no-underline opacity-0 transition group-hover:opacity-100 [@media(any-hover:none)]:opacity-100"
 	href="/">POSTS /</a
 >
