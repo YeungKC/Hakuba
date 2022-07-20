70a8ed9b666adbcab2ffa58bf5420edd320619eb
YeungKC <I@YeungKC.com>
2022-07-20 22:04:10 +0800

    comment of date format

--- src/lib/helper/readableDate.ts
+++ src/lib/helper/readableDate.ts
@@ -1,5 +1,6 @@
 import { TIME_ZONE } from '../constants';
 
+// Date format in Swedish is the same as ISO
 export const readableDate = (dateText: string) =>
 	new Date(dateText).toLocaleDateString('sv', {
 		timeZone: TIME_ZONE,
