1438ffc5a2ec76462edb21a68e50efa53eb4c710
YeungKC <I@YeungKC.com>
2022-07-25 07:57:03 +0800

    Fix table overflow

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -3,6 +3,9 @@
 	export let lang: string | undefined = undefined;
 </script>
 
-<article class="prose prose-slate prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg" {lang}>
+<article
+	class="prose-slate prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg  prose-table:block prose-table:w-max prose-table:max-w-full prose-table:overflow-auto"
+	{lang}
+>
 	<slot />
 </article>
