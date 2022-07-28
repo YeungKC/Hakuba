780c49a4e73fc5d196d8e7f01228a62d9201a9a3
YeungKC <I@YeungKC.com>
2022-07-29 07:11:55 +0800

    Prevent prose th text will not be wrapped.

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <article
-	class="prose prose-slate prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg  prose-table:block prose-table:overflow-auto"
+	class="prose prose-slate prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg  prose-table:block prose-table:overflow-auto prose-th:whitespace-pre"
 	{lang}
 >
 	<slot />
